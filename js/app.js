6b00fccf1a3983b04cd9cbf9d87df95b8a42ea0c
Paulina Szelemetko <p.szelemetko@gmail.com>
2018-04-11 16:36:00 +0200

    change comments

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
-/*
- * Create an array that holds all of cards
- */
+// MEMORY GAME
+
+// Create an array that holds all of cards
+ 
 var array = [
-    "fa-diamond",
-    "fa-paper-plane-o",
-    "fa-anchor",
-    "fa-bolt",
-    "fa-cube",
-    "fa-leaf",
-    "fa-bicycle",
-    "fa-bomb",
-    "fa-diamond",
-    "fa-paper-plane-o",
-    "fa-anchor",
-    "fa-bolt",
-    "fa-cube",
-    "fa-leaf",
-    "fa-bicycle",
-    "fa-bomb"
+    'fa-diamond',
+    'fa-paper-plane-o',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-cube',
+    'fa-leaf',
+    'fa-bicycle',
+    'fa-bomb',
+    'fa-diamond',
+    'fa-paper-plane-o',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-cube',
+    'fa-leaf',
+    'fa-bicycle',
+    'fa-bomb'
 ]
 
 // Global variables
@@ -26,18 +27,19 @@ var moves = 0;
 var seconds = 1;
 var minutes = 0;
 var start = false; 
-var matchCards = document.getElementsByClassName("match");
-var modal = document.getElementsByClassName("modal")[0];
-var modalContent = document.getElementsByClassName("modal-content-text")[0];
+var matchCards = document.getElementsByClassName('match');
+var modal = document.getElementsByClassName('modal')[0];
+var modalContent = document.getElementsByClassName('modal-content-text')[0];
 
 
-//DOM Loading
+// Waiting for DOM Loading
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
     prepareDeck(array);
 }) 
 
-//Prepare deck
+// Prepare deck function 
+
 function prepareDeck(array) {
     removeCards();
     shuffle(array);
@@ -45,23 +47,23 @@ function prepareDeck(array) {
 }
 
  function removeCards () {
-    var deck = document.getElementsByClassName("deck")[0];
+    var deck = document.getElementsByClassName('deck')[0];
     while (deck.firstChild) {
         deck.removeChild(deck.firstChild);
     }
 }
 
-//Create cards and compare open cards
+// Create cards
 
-function createCard (iconName) {
-    var card = document.createElement("li");
-    var icon = document.createElement("i");
+function createCard (iconName) {  
+    var card = document.createElement('li');
+    var icon = document.createElement('i');
 
     card.appendChild(icon);
-    card.classList.add("card");
-    icon.classList.add("fa", iconName);
+    card.classList.add('card');
+    icon.classList.add('fa', iconName);
 
-    card.addEventListener("click", function() {
+    card.addEventListener('click', function() {
         
         if (start === false){
              timeStart(seconds);
@@ -70,39 +72,41 @@ function createCard (iconName) {
        
         removeStars(moves);
 
-        if(this.classList.contains("match") || this.classList.contains("show")) {
+        // Compare open cards
+
+        if(this.classList.contains('match') || this.classList.contains('show')) {
             return;
         }
         
-        var openCards = document.getElementsByClassName("open");
+        var openCards = document.getElementsByClassName('open');
 
         if (openCards.length === 2) {
-            openCards.item(1).classList.remove("show","open");
-            openCards.item(0).classList.remove("show","open");
-            this.classList.add("show", "open");
+            openCards.item(1).classList.remove('show','open');
+            openCards.item(0).classList.remove('show','open');
+            this.classList.add('show', 'open');
             return;
         }
 
         if (openCards.length === 1) {
            if(compareCards(openCards[0].firstChild, this.firstChild)) {
-               openCards[0].classList.add("match");
-               openCards[0].classList.remove("show","open");
-               this.classList.add("match");
+               openCards[0].classList.add('match');
+               openCards[0].classList.remove('show','open');
+               this.classList.add('match');
            } else {
-               this.classList.add("show", "open");
+               this.classList.add('show', 'open');
            }
 
            moves++;
-           document.getElementsByClassName("moves")[0].textContent = moves;
+           document.getElementsByClassName('moves')[0].textContent = moves;
 
            return;
         } 
         
         if (openCards.length === 0) {
-            this.classList.add("show", "open");
+            this.classList.add('show', 'open');
         }
 
-        // Ending game 
+        // Ending game (when winning)
 
         if (matchCards.length === 15) {
             endingGame();
@@ -115,6 +119,8 @@ function createCard (iconName) {
     return card;
 }
 
+// Compare cards function 
+
 function compareCards(first, second) {
     for (var i = 0; i < first.classList.length; i++) {
         if (!second.classList.contains(first.classList[i])) {      
@@ -124,9 +130,10 @@ function compareCards(first, second) {
     return true;
 }
 
+// Add cards function 
 
 function addCards (cardNames) {
-    var deck = document.getElementsByClassName("deck")[0];
+    var deck = document.getElementsByClassName('deck')[0];
     var card;
 
     for (var i=0; i<cardNames.length; i++) {
@@ -151,40 +158,40 @@ function shuffle(array) {
     return array;
 }
 
- // Restart 
+ // Restart function
 
-var restart = document.getElementsByClassName("restart")[0];
+var restart = document.getElementsByClassName('restart')[0];
 
-restart.addEventListener("click", function restart() {
+restart.addEventListener('click', function restart() {
     prepareDeck(array);
     moves=0;
     start = false;
     minutes = 0;
     seconds = 0;
-    timer.innerHTML = "00:00";
-    document.getElementsByClassName("moves")[0].textContent = moves;
+    timer.innerHTML = '00:00';
+    document.getElementsByClassName('moves')[0].textContent = moves;
     clearInterval(interwal);
-});
+})
 
 
-// Remove stars
+// Remove stars function
 
 function removeStars(moves) {
-    var stars = document.getElementsByClassName("stars")[0];
+    var stars = document.getElementsByClassName('stars')[0];
     if (moves === 9 || moves === 19 || moves === 29) {
         stars.removeChild(stars.childNodes[0]);
     }
     return;
 }
 
-//Timer
+//Timer (time-start and time-create function)
 
 var interwal;
 var timer;
 
 function timeStart(seconds) {
     
-    timer = document.getElementsByClassName("timer")[0];
+    timer = document.getElementsByClassName('timer')[0];
     interwal = setInterval(createTime, 1000);
 }
 
@@ -196,43 +203,43 @@ function createTime() {
         seconds = 0;
     }
     if (seconds < 10) {
-        timer.innerHTML = "0" + minutes + ":" + "0" + seconds;
+        timer.innerHTML = '0' + minutes + ':' + '0' + seconds;
     }
     if (seconds >= 10) {
-        timer.innerHTML = "0" + minutes + ":"  + seconds;
+        timer.innerHTML = '0' + minutes + ':'  + seconds;
     }
     if (minutes >=10) {
-        timer.innerHTML = minutes + ":" + "0" + seconds;
+        timer.innerHTML = minutes + ':' + '0' + seconds;
     }
     }
 
 
-// Ending game function
+// Ending game function (show modal window)
 
 function endingGame() {
-    var timer = document.getElementsByClassName("timer")[0].textContent;
-    modal.style.display = "block";
-    modalContent.innerHTML = "<h1>Congratulations! You won!</h1> Your time: " + timer + "<br>" +  "Your moves: " + moves;
+    var timer = document.getElementsByClassName('timer')[0].textContent;
+    modal.style.display = 'block';
+    modalContent.innerHTML = '<h1>Congratulations! You won!</h1> Your time: ' + timer + '<br>' +  'Your moves: ' + moves;
 
     
 }
 
-//Play again button (modal window)
+//Play again function (button in modal window)
 
 function playAgain() {
-    var button = document.getElementsByClassName("play-again")[0];
-    button.addEventListener("click", function() {
-        modal.style.display = "none";
+    var button = document.getElementsByClassName('play-again')[0];
+    button.addEventListener('click', function() {
+        modal.style.display = 'none';
         prepareDeck(array);
         moves=0;
         start = false;
         minutes = 0;
         seconds = 0;
-        timer.innerHTML = "00:00";
-        document.getElementsByClassName("moves")[0].textContent = moves;
+        timer.innerHTML = '00:00';
+        document.getElementsByClassName('moves')[0].textContent = moves;
         clearInterval(interwal);
      
-    });
+    })
 }
 
  
